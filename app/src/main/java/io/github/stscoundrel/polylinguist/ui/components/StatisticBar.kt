ae68a9157ac71a829b481b10abca8e9628a2b1fe
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-03-23 21:29:08 +0200

    Refactor HorizontalBar to StatisticsBar

--- app/src/main/java/io/github/stscoundrel/polylinguist/ui/components/StatisticBar.kt
+++ app/src/main/java/io/github/stscoundrel/polylinguist/ui/components/StatisticBar.kt
@@ -6,26 +6,29 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import io.github.stscoundrel.polylinguist.domain.Statistic
 import io.github.stscoundrel.polylinguist.ui.utils.createColorFromHex
 
 @Composable
-fun HorizontalBar(
-    percentage: Double,
+fun StatisticBar(
+    statistic: Statistic,
     largestPercentage: Double,
-    colorHex: String,
     modifier: Modifier = Modifier
 ) {
-    val clampedPercentage = (percentage / largestPercentage).coerceIn(0.0, 1.0)
+    val clampedPercentage = (statistic.percentage / largestPercentage).coerceIn(0.0, 1.0)
 
     Box(
         modifier = modifier
-            .height(8.dp)
-            .padding(5.dp, 0.dp)
+            .height(10.dp)
+            .padding(10.dp, 0.dp)
+            .clip(shape = RoundedCornerShape(15.dp, 15.dp, 15.dp, 15.dp))
             .fillMaxWidth(clampedPercentage.toFloat())
-            .background(createColorFromHex(colorHex))
+            .background(createColorFromHex(statistic.color))
     )
-    Spacer(modifier = modifier.height(5.dp))
+    Spacer(modifier = modifier.height(8.dp))
 }
