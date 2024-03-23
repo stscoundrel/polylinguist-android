df946ea6eb18be9b1d419e18c54ec0d53e727483
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-03-23 21:19:32 +0200

    Extract Statistics component from screen

--- app/src/main/java/io/github/stscoundrel/polylinguist/ui/screens/CurrentStatisticsScreen.kt
+++ app/src/main/java/io/github/stscoundrel/polylinguist/ui/screens/CurrentStatisticsScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -16,9 +15,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import io.github.stscoundrel.polylinguist.ui.components.HorizontalBar
 import io.github.stscoundrel.polylinguist.ui.components.LoadingSpinner
-import io.github.stscoundrel.polylinguist.ui.utils.formatPresentationDate
+import io.github.stscoundrel.polylinguist.ui.components.Statistics
 import io.github.stscoundrel.polylinguist.ui.viewmodels.CurrentStatisticsViewModel
 
 
@@ -40,26 +38,7 @@ fun CurrentStatisticsScreen(
         }
 
         statistics?.let { stats ->
-            Text(text = "Date: ${formatPresentationDate(stats.date)}")
-            Spacer(modifier = Modifier.height(8.dp))
-
-            val largestPercentage = stats.statistics.maxByOrNull { it.percentage }!!
-                .percentage
-
-            stats.statistics.sortedByDescending { it.percentage }
-                .forEachIndexed { index, statistic ->
-                    val presentationIndex = index + 1
-                    val roundedPercentage = "%.2f".format(statistic.percentage)
-                    Text(
-                        text = "$presentationIndex. ${statistic.language} - ${roundedPercentage}%",
-                        modifier = Modifier.padding(bottom = 2.dp)
-                    )
-                    HorizontalBar(
-                        percentage = statistic.percentage,
-                        largestPercentage = largestPercentage,
-                        colorHex = statistic.color
-                    )
-                }
+            Statistics(stats)
         }
 
         if (!statsViewModel.hasUpToDateStatistics()) {
