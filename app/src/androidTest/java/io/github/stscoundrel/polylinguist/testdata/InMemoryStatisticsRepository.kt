fc47265488386ace6517f6c834209ad91e715753
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-03-23 14:38:11 +0200

    AndroidTest statistics repo: add artificial delays
    Better matches real implementation & should remedy a particular issue with flaky tests in CI.

--- app/src/androidTest/java/io/github/stscoundrel/polylinguist/testdata/InMemoryStatisticsRepository.kt
+++ app/src/androidTest/java/io/github/stscoundrel/polylinguist/testdata/InMemoryStatisticsRepository.kt
@@ -3,6 +3,7 @@ package io.github.stscoundrel.polylinguist.testdata
 import io.github.stscoundrel.polylinguist.domain.Statistic
 import io.github.stscoundrel.polylinguist.domain.Statistics
 import io.github.stscoundrel.polylinguist.domain.StatisticsRepository
+import kotlinx.coroutines.delay
 import java.time.LocalDate
 
 class InMemoryStatisticsRepository(
@@ -12,10 +13,12 @@ class InMemoryStatisticsRepository(
     val statistics: MutableMap<LocalDate, List<Statistic>> = mutableMapOf()
 
     override suspend fun getCurrent(): Statistics {
+        delay(1500)
         return currentStatistics
     }
 
     override suspend fun getLatest(): Statistics {
+        delay(1500)
         return latestStatistics
     }
 
