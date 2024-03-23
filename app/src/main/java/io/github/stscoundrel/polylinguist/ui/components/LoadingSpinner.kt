b3f8425b34fbab5433bd4e0ec6f00c37935cc68c
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-03-23 19:07:56 +0200

    Add loading spinner component
    Use the spinner instead of the placeholder "loading" texts.

---
+++ app/src/main/java/io/github/stscoundrel/polylinguist/ui/components/LoadingSpinner.kt
@@ -0,0 +1,20 @@
+package io.github.stscoundrel.polylinguist.ui.components
+
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun LoadingSpinner() {
+
+    CircularProgressIndicator(
+        modifier = Modifier.width(64.dp),
+        color = MaterialTheme.colorScheme.secondary,
+        trackColor = MaterialTheme.colorScheme.surfaceVariant,
+    )
+
+}
