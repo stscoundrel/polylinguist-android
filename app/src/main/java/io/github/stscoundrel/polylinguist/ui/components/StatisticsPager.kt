73f47aa70bb310a0bc3f007221de3e71feb8da7e
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-04-07 09:50:43 +0300

    Add StatisticsHistory screen
    No navigation yet, just crude flag to output in main activity

---
+++ app/src/main/java/io/github/stscoundrel/polylinguist/ui/components/StatisticsPager.kt
@@ -0,0 +1,58 @@
+package io.github.stscoundrel.polylinguist.ui.components
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import io.github.stscoundrel.polylinguist.domain.Statistics
+
+@Composable
+@OptIn(ExperimentalFoundationApi::class)
+fun StatisticsPager(
+    pagerState: PagerState,
+    statistics: List<Statistics>,
+    modifier: Modifier = Modifier
+) {
+    HorizontalPager(state = pagerState) { page ->
+        Column {
+            Statistics(statistics[page])
+        }
+    }
+
+    Spacer(modifier = modifier.height(16.dp))
+
+    Row(
+        Modifier
+            .wrapContentHeight()
+            .fillMaxWidth()
+            .padding(bottom = 8.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        repeat(pagerState.pageCount) { iteration ->
+            val color = if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+            Box(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .clip(CircleShape)
+                    .background(color)
+                    .size(10.dp)
+            )
+        }
+    }
+}
