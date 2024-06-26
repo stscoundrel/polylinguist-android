24b05bf341660b4661444f3f14758c0e06c334b7
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-06-26 10:45:17 +0300

    Add comparison periods to history view
    Simple comparison of how percentages have changed since the last available period.

--- app/src/main/java/io/github/stscoundrel/polylinguist/ui/components/Statistics.kt
+++ app/src/main/java/io/github/stscoundrel/polylinguist/ui/components/Statistics.kt
@@ -2,7 +2,6 @@ package io.github.stscoundrel.polylinguist.ui.components
 
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,7 +10,7 @@ import io.github.stscoundrel.polylinguist.domain.Statistics
 import io.github.stscoundrel.polylinguist.ui.utils.formatPresentationDate
 
 @Composable
-fun Statistics(statistics: Statistics) {
+fun Statistics(statistics: Statistics, comparisons: Statistics? = null) {
     val sortedStats = statistics.statistics.sortedByDescending { it.percentage }
     val largestPercentage = sortedStats.first().percentage
 
@@ -20,14 +19,13 @@ fun Statistics(statistics: Statistics) {
 
     sortedStats.forEachIndexed { index, statistic ->
         val presentationIndex = index + 1
-        val roundedPercentage = "%.2f".format(statistic.percentage)
-        Text(
-            text = "$presentationIndex. ${statistic.language} - ${roundedPercentage}%",
-            modifier = Modifier.padding(bottom = 2.dp)
-        )
+        val comparison =
+            comparisons?.statistics?.first { it.language == statistic.language }
+        StatisticTitle(presentationIndex, statistic, comparison)
         StatisticBar(
             statistic = statistic,
             largestPercentage = largestPercentage,
         )
     }
 }
+
